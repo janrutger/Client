ef297630fac83669af4e1ea28671b09172c0efbb
janrutger <janrutger@jaruku.nl>
2021-04-23 17:00:39 +0200

    Fitrst JS

---
+++ index.js
@@ -0,0 +1,7 @@
+function refresh(){
+    console.log("refresh")
+    document.getElementById("parmName").innerHTML="TEMPERATURE"
+    document.getElementById("parmValue").innerHTML=22.4
+    document.getElementById("parmMean").innerHTML=21.3
+    document.getElementById("parmMarker").innerHTML=21.9
+}
