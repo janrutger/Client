7ec9c1ddb76c6c9a06bef8e54692bcb5337322e3
janrutger <janrutger@jaruku.nl>
2021-04-23 19:24:22 +0200

    More JS, fetch json

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 function refresh(){
-    console.log("refresh")
+    console.log("Func refresh")
+    let url = "http://rest:5000/slice/json/now/2/5/Amsterdam/temperature"
+
+
+    fetch(url)
+    .then(res => res.json())
+    .then((out) => {
+        //console.log('Output: ', out["ANSWER"]);
+        answer = out['ANSWER']
+        console.log(answer)
+        }).catch(err => console.error(err));
+}   
+
+function update(){
+    console.log("Func update")
     document.getElementById("parmName").innerHTML="TEMPERATURE"
     document.getElementById("parmValue").innerHTML=22.4
     document.getElementById("parmMean").innerHTML=21.3
     document.getElementById("parmMarker").innerHTML=21.9
+}
+
+
+
+function refreshButton(){
+    refresh()
+    update()
+    //console.log(a)
 }
