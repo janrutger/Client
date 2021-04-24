0a58584f0b29708a517fc3a76b7a1d8deea33748
janrutger <janrutger@jaruku.nl>
2021-04-24 21:56:16 +0200

    Het werkt

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-async function refresh(){
+function refresh(){
     console.log("Func refresh")
     let url = "http://rest:5000/slice/json/now/2/5/Amsterdam/temperature"
     //let url = "ServerResponse.json"
 
-
-    /*fetch(url)
+    fetch(url)
     .then(res => res.json())
-    .then((out) => {
-        //console.log('Output: ', out["ANSWER"]);
-        answer = out['ANSWER']
-        console.log(answer)
-        return answer
-        }).catch(err => console.error(err)); */
+    .then(j => {console.log(j)
+        return j})
+    .then(j => update(j["ANSWER"]) );
     
-    let response = await fetch(url)
-    let data = await response.json()
-    console.log(data["ANSWER"])
-    return data
-
 }   
 
-function update(){
+function update(info){
     console.log("Func update")
-    document.getElementById("parmName").innerHTML="TEMPERATURE"
-    document.getElementById("parmValue").innerHTML=22.4
-    document.getElementById("parmMean").innerHTML=21.3
-    document.getElementById("parmMarker").innerHTML=21.9
+    console.log(info)
+
+    //value = info["VALUE_LAST"]
+    console.log(Object.keys(info["VALUE_LAST"])[0])
+    parm = Object.keys(info["VALUE_LAST"])[0]
+    //val = info["VALUE_LAST"]
+    //console.log(val)
+    //console.log(val["T"])
+    //for (key of val){   
+     //   console.log(key[0])
+    //}
+
+    document.getElementById("parmName").innerHTML=info["PARAMETER"]
+    document.getElementById("parmValue").innerHTML=info["VALUE_LAST"][parm]
+    document.getElementById("parmMean").innerHTML=info["VALUE_AVERAGE"][parm]
+    document.getElementById("parmMarker").innerHTML=info["VALUE_N_AVERAGE"][parm]
 }
 
 
 
 function refreshButton(){
-    var y = refresh()
-    console.log(y)
-    update()
-    //console.log(a)
+    refresh()
 }
